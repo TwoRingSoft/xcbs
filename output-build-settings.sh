963e38bd90615323b23190a63046b4694ca064b3
Andrew McKnight <andrew@layer.com>
2017-02-04 16:06:58 -0500

    update and use output-build-settings.sh to generate test baseline output

--- output-build-settings.sh
+++ output-build-settings.sh
@@ -7,16 +7,19 @@ OIFS="$IFS"
 IFS=$'\n'
 
 XCDANGER_SCHEMES=`find "${XCDANGER_PROJECT}"/xcshareddata -name "*.xcscheme"`
+XCDANGER_CONFIGURATIONS=`xcodebuild -project "${XCDANGER_PROJECT}" -list | ruby list-configurations.rb`
 
 XCDANGER_OUTPUT_PATH="${XCDANGER_PROJECT}/../.xcdanger/"
-mkdir "${XCDANGER_OUTPUT_PATH}"
+mkdir -p "${XCDANGER_OUTPUT_PATH}"
 
 for XCDANGER_SCHEME_PATH in ${XCDANGER_SCHEMES[@]}; do
     
     # replace spaces in name with escape-quoted space characters, so that basename doesn't break the to new lines either
     XCDANGER_SCHEME_NAME=`echo "${XCDANGER_SCHEME_PATH}" | sed s/' '/'\"\ \"'/g | xargs basename | sed s/'.xcscheme'//g`
     
-    sh -c "xcodebuild -project \"${XCDANGER_PROJECT}\" -scheme \"${XCDANGER_SCHEME_NAME}\" -showBuildSettings > \"${XCDANGER_OUTPUT_PATH}/${XCDANGER_SCHEME_NAME}\".settings.lock"
+    for XCDANGER_CONFIGURATION in ${XCDANGER_CONFIGURATIONS[@]}; do
+        sh -c "xcodebuild -project \"${XCDANGER_PROJECT}\" -scheme \"${XCDANGER_SCHEME_NAME}\" -configuration \"${XCDANGER_CONFIGURATION}\" -showBuildSettings > \"${XCDANGER_OUTPUT_PATH}/${XCDANGER_SCHEME_NAME}.${XCDANGER_CONFIGURATION}\".settings.lock"
+    done
 done
 
 # replace the old file separator character
