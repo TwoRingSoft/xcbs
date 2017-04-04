0f2c2d1fa6c99a56264fe31544c2099c3ade368e
Andrew McKnight <amcknight2718@gmail.com>
2017-04-04 00:52:49 -0400

    add usage and version switches; improve comment; add xcode file

--- scripts/sort-settings-by-values.rb
+++ scripts/sort-settings-by-values.rb
@@ -1,6 +1,6 @@
 # given the list of "BUILD_SETTING_NAME = value" strings in provided file, sort them by their value portion and output in form "value = BUILD_SETTING_NAME" 
 #
-# This helps to easily identify the roots of build setting values to unexpand in other values, thus avoiding generating diffs on different developers' machines.
+# This is a utility to help identify the roots of build setting values to unexpand in other values, thus avoiding generating diffs on different developers' machines. This script is not meant for deployment to users' machines.
 
 keysAndValues = Hash.new
 
